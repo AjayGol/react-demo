16979619c9d312ead1c66b3210a36fa1a9e7768d
= <=>
2022-07-17 19:15:29 +0530

    react demo

---
+++ src/screen/Register.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import React, { useState } from "react";
+
+
+function Register() {
+    return (
+        <div className="app">
+            <div className="login-form">
+                <div className="title">Sign22222 In</div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
